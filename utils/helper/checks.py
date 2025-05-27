292a62b2737cdd681736d930fafefc5a22095fdc
ryuzinoh <ryuzinoh@users.noreply.github.com>
2025-05-27 14:49:57 +0545

    feat: add the authentication helper for user

---
+++ utils/helper/checks.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+from models.user import UserModel
+
+u_model = UserModel()
+
+def is_registered():
+    async def predicate(ctx):
+        if not u_model.user_exists(ctx.author.id):
+            await ctx.send("You must be registered!!, try registereing with `register` command first.")
+            return False
+        return True
+    return commands.check(predicate)
+
+
+
